616e6de4b10abeb332edf1f65ac9876505db6831
Light1121 <light1121.lin@gmail.com>
2025-03-30 11:49:44 +1100

    updated config for yml and Dockerfile, enable linking to mongodb, build and start docker container, switced activation from wsgi.py to app.py

--- app/config.py
+++ app/config.py
@@ -1,15 +1,13 @@
-# from flask import Config
+from flask import Config
 
-# class Config(Config):
-#     ENABLE_UTC = True
-#     APP_NAME = "founder-bot"
-    
-
-#     MONGODB_HOST = "127.0.0.1"
-#     MONGODB_PORT = 27018
-#     MONGODB_DB = "founderbot-api"
-#     MONGODB_USERNAME = "ROOTchatbot"
-#     MONGODB_PASSWORD = "ROOTchatbot"
+class Config(Config):
+    ENABLE_UTC = True
+    APP_NAME = "founder-bot"
+    MONGODB_HOST = "127.0.0.1"
+    MONGODB_PORT = 27017
+    MONGODB_DB = "founderbot-api"
+    MONGODB_USERNAME = "ROOTchatbot"
+    MONGODB_PASSWORD = "ROOTchatbot"
 
 # import os
 
@@ -19,13 +17,13 @@
     
 #     MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://127.0.0.1:27018/founderbot-api")
 
-import os
+# import os
 
-class Config:
-    ENABLE_UTC = True
-    APP_NAME = "founder-bot"
+# class Config:
+#     ENABLE_UTC = True
+#     APP_NAME = "founder-bot"
     
-    MONGODB_URI = os.getenv(
-        "MONGODB_URI", 
-        "mongodb://ROOTchatbot:ROOTchatbot@mongo_chatbot:27017/founderbot-api?authSource=admin"
-    )
+#     MONGODB_URI = os.getenv(
+#         "MONGODB_URI", 
+#         "mongodb://ROOTchatbot:ROOTchatbot@mongo_chatbot:27017/founderbot-api?authSource=admin"
+#     )
